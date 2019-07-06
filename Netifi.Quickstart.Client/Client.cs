837783cf58c3a68f54c1c2784e4e52b6507ee872
Ryland Degnan <ryland@netifi.com>
2019-07-06 15:55:42 -0700

    Initial commit

---
+++ Netifi.Quickstart.Client/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RSocket;
+using RSocket.Transports;
+using Com.Netifi.Quickstart.Service;
+
+namespace Netifi.Quickstart
+{
+    public class Client
+    {
+        static async Task Main(string[] args)
+        {
+            // Build Netifi Broker Connection
+            var accessKey = 9007199254740991;
+            var accessToken = "kTBDVtfRBO4tHOnZzSyY5ym2kfY=";
+            var transport = new SocketTransport("tcp://localhost:8001/");
+            var client = new Netifi.Broker.Client.BrokerClient(accessKey, accessToken, null, null, "quickstart.clients", "client", 0, new SortedDictionary<string, string>(), transport, RSocketOptions.Default);
+
+            // Connect to Netifi Platform
+            await client.ConnectAsync();
+
+            // Create Client to Communicate with the HelloService (included example service)
+            var group = client.Group("quickstart.services.helloservices", new SortedDictionary<string, string>());
+            var helloService = new HelloService.HelloServiceClient(group);
+
+            // Create Request to HelloService
+            var request = new HelloRequest();
+            request.Name = "World";
+
+            Console.WriteLine("Sending 'World' to HelloService...");
+
+            // Call the HelloService
+            var response = await helloService.SayHello(request, new ReadOnlySequence<byte>());
+            Console.WriteLine(response.Message);
+        }
+    }
+}
---
+++ Netifi.Quickstart.Server/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RSocket;
+using RSocket.Transports;
+using Com.Netifi.Quickstart.Service;
+
+namespace Netifi.Quickstart
+{
+    public class Server
+    {
+        static async Task Main(string[] args)
+        {
+			var serviceName = "helloservice-" + Guid.NewGuid().ToString();
+
+			// Build Netifi Broker Connection
+			var accessKey = 9007199254740991;
+			var accessToken = "kTBDVtfRBO4tHOnZzSyY5ym2kfY=";
+			var transport = new SocketTransport("tcp://localhost:8001/");
+			var client = new Netifi.Broker.Client.BrokerClient(accessKey, accessToken, null, null, "quickstart.services.helloservices", "serviceName", 0, new SortedDictionary<string, string>(), transport, RSocketOptions.Default);
+
+			// Connect to Netifi Platform
+			await client.ConnectAsync();
+
+
+		}
+    }
+}
